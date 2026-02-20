039ecbbb120a71759d1703df7a61a9e052dce330
hamzahshazadchattha <greatchattha@gmail.com>
2026-02-20 09:58:41 +0500

    Add comprehensive documentation covering technical explanation, API, architecture, CloudWatch queries, and code standards, while removing temporary test scripts.

--- test-cf3.py
+++
@@ -1,9 +0,0 @@
-import json
-with open('.serverless/cloudformation-template-update-stack.json', 'r') as f:
-    t = json.load(f)
-
-# API Gateway V2 Authorizer IdentitySource can be very finicky
-t['Resources']['HttpApiJwtAuthorizer']['Properties']['IdentitySource'] = ["$request.header.Authorization"]
-# Try to make sure it's valid CF
-with open('.serverless/cloudformation-template-update-stack-test.json', 'w') as f:
-    json.dump(t, f, indent=2)
